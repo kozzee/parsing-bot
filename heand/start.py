8edd52a06e3bd81053711815e1fed7d8210dc60c
kozzee <config@email.com>
2024-12-04 14:32:02 +0300

    это ответ на \start

---
+++ heand/start.py
@@ -0,0 +1,9 @@
+from aiogram import Router, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+start_rt = Router()
+
+@start_rt.message(CommandStart())
+async def star_cmd(message: Message):
+    await message.answer('Мы начнем парсировать вакансии на HH.ru. Ниже Кнопки для дальнейшей работы:')
