1246d54c5a3a1b300ec00e31b9689ddceaca2078
kozzee <config@email.com>
2024-12-05 18:51:29 +0300

    парсинг вакансий

--- pars/pars_rout.py
+++ pars/pars_rout.py
@@ -1,9 +1,23 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from pars.api_hh import extract_vacancies, get_number_of_vacation
+from pars import api_hh
 
 pars_router = Router()
 
 @pars_router.message(F.text == '🔍 Парсинг')
 async def pars(message : Message):
-    await message.answer('Начинаем парсить вакансии')
+    await message.answer('Начинаем парсить вакансии!')
+    await message.answer('🔎')
+
+    number_vac = get_number_of_vacation()
+    await message.answer(f'Найдено {number_vac} вакансий. Показать с первой страницы?')
+
+@pars_router.message(F.text == 'Покажи')
+async def vacant_show(message: Message):
+    vacancies = extract_vacancies()
+    for i in vacancies:
+        answer = f"{i['title']}\n{i['employer']}\n{i['description']}\n{i['link']}"
+        await message.answer(answer)
+
